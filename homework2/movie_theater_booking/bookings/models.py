33f6b02d102157187d36c8a068a6e75f55ac2443
John-Camargo <jecamargo2003@hotmail.com>
2025-09-26 18:46:35 +0000

    Fixing/modifying models.py to account for foreigh key (other model) usage in the Booking model.

--- homework2/movie_theater_booking/bookings/models.py
+++ homework2/movie_theater_booking/bookings/models.py
@@ -5,18 +5,27 @@ from django.db import models
 # movie model with title, descriptions, release date, and duration fields
 class Movie(models.Model):
     title = models.CharField(max_length = 40)
-    descriptions = models.CharField(max_length = 200)
+    description = models.TextField()
     release_date = models.DateField()
-    duration = models.IntegerField()
+    duration = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.title
 
 # seat model with seat number and booking status
 class Seat(models.Model):
-    seat_number = models.IntegerField()
-    booking_status = models.CharField(max_length = 20)
+    seat_number = models.PositiveIntegerField()
+    booked = models.BooleanField(default = False)
+
+    def __str__(self):
+        return f"Seat {self.seat_number} - {'Booked' if self.is_booked else 'Available'}"
 
 # booking model with movie, seat, user, and booking date fields
 class Booking(models.Model):
-    movie = models.CharField(max_length = 40)
-    seat = models.IntegerField()
+    movie = models.ForeignKey(Movie, on_delete = models.CASCADE)
+    seat = models.ForeignKey(Seat, on_delete = models.CASCADE)
     user = models.CharField(max_length = 50)
     booking_date = models.DateField()
+
+    def __str__(self):
+        return f"{self.user} booked {self.seat} for {self.movie}"
