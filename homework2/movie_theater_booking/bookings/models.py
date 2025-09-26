5bf01371d4c7093d95a547e43c289fac1d3cc362
John-Camargo <jecamargo2003@hotmail.com>
2025-09-26 18:10:20 +0000

    Creating the models for the bookong app (3.2).

--- homework2/movie_theater_booking/bookings/models.py
+++ homework2/movie_theater_booking/bookings/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+# movie model with title, descriptions, release date, and duration fields
+class Movie(models.Model):
+    title = models.CharField(max_length = 40)
+    descriptions = models.CharField(max_length = 200)
+    release_date = models.DateField()
+    duration = models.IntegerField()
+
+# seat model with seat number and booking status
+class Seat(models.Model):
+    seat_number = models.IntegerField()
+    booking_status = models.CharField(max_length = 20)
+
+# booking model with movie, seat, user, and booking date fields
+class Booking(models.Model):
+    movie = models.CharField(max_length = 40)
+    seat = models.IntegerField()
+    user = models.CharField(max_length = 50)
+    booking_date = models.DateField()
