e8af29e6209ac7a4542ed43088657bfd3a95325e
John-Camargo <jecamargo2003@hotmail.com>
2025-10-04 18:45:05 +0000

    Add admin panel to manually add movies to app.

--- homework2/movie_theater_booking/bookings/admin.py
+++ homework2/movie_theater_booking/bookings/admin.py
@@ -1,3 +1,21 @@
+# bookings/admin.py
 from django.contrib import admin
+from .models import Movie, Seat, Booking
 
-# Register your models here.
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ("title", "release_date", "duration")
+    search_fields = ("title", "description")
+    list_filter = ("release_date",)
+
+@admin.register(Seat)
+class SeatAdmin(admin.ModelAdmin):
+    list_display = ("seat_number", "booked")
+    list_filter = ("booked",)
+    search_fields = ("seat_number",)
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ("user", "movie", "seat", "booking_date")
+    list_filter = ("booking_date", "movie")
+    search_fields = ("user",)
